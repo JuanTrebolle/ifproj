8d019357a9d6877a77ab38395492de035d3d8b66
JuanTrebolle <juan.itrebolle@gmail.com>
2024-04-14 14:19:11 +0200

    Added getters & setters + variable types

--- AuctionHouse.java
+++ AuctionHouse.java
@@ -2,11 +2,36 @@ import java.util.ArrayList;
 
 class AuctionHouse {
     private String houseName;
-    private ArrayList customers;
-    private ArrayList items;
+    private ArrayList<Bidder> customers; // Not sure if the type is "Bidder" or if there should be a "Customer" class
+    private ArrayList<Item> items;
 
-    AuctionHouse(String houseName, ArrayList customers, ArrayList items){
-
+    AuctionHouse(String houseName, ArrayList<Bidder> customers, ArrayList<Item> items){
+        this.houseName = houseName;
+        this.customers = customers;
+        this.items = items;
     }
 
+    public String getHouseName() {
+        return houseName;
+    }
+
+    public void setHouseName(String houseName) {
+        this.houseName = houseName;
+    }
+
+    public ArrayList<Bidder> getCustomers() {
+        return customers;
+    }
+
+    public void setCustomers(ArrayList<Bidder> customers) {
+        this.customers = customers;
+    }
+
+    public ArrayList<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(ArrayList<Item> items) {
+        this.items = items;
+    }
 }
--- Bidder.java
+++ Bidder.java
@@ -1,7 +1,24 @@
 import java.util.ArrayList;
+import java.util.UUID;
 
 class Bidder {
-    private int id;
-    private ArrayList houses;
+    private String id;
+    private ArrayList<ActionHouse> houses;
 
+    public Bidder(int id, ArrayList<AuctionHouse> houses) {
+        this.id = UUID.randomUUID().toString();;
+        this.houses = houses;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public ArrayList<ActionHouse> getHouses() {
+        return houses;
+    }
+
+    public void setHouses(ArrayList<ActionHouse> houses) {
+        this.houses = houses;
+    }
 }
